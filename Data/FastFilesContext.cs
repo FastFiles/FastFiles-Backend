a477e4ad414385ef1abee34745089ae1f32f3de6
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-06-25 21:34:41 -0500

    feat: Modelo y DbSet

--- Data/FastFilesContext.cs
+++ Data/FastFilesContext.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using FastFiles.Models;
+
 namespace FastFiles.Data
 {
     public class FastFilesContext : DbContext
     {
-        public FastFilesContext(DbContextOptions<FastFilesContext> options) : base(options)
-        {
-
-        }
-        //Agregamos los modelos.
+        public FastFilesContext(DbContextOptions<FastFilesContext> options) : base(options) {}
+        
+        public DbSet<User> Users { get; set; }
     }
 }
