8b131955416b1adfdd5e1d5d5a61cc199788a432
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-06-25 21:30:44 -0500

    fix: Configs Faltantes

--- Program.cs
+++ Program.cs
@@ -3,48 +3,68 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using FastFiles.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Controllers
+builder.Services.AddControllers();
+
+// Cors 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 
 //Agregamos la configiuracion de Jwt
 builder.Services.AddAuthentication(opt => {
     opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
+
 .AddJwtBearer(options =>
- {
+{
     options.TokenValidationParameters = new TokenValidationParameters{
         ValidateIssuer = true,
         ValidateAudience = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        ValidIssuer = "", //Agregamos variabel de entorno Jwt
+        ValidIssuer = "", //Agregamos variable de entorno Jwt
         ValidAudience = "",
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mcrfnief3ie84r4hrffrñ@dnrcnjfcnfnjcnjr232N"))
     };
 });
-//Agregamos la configuración de la base de datos
 
-builder.Services.AddDbContext<FastFilesContext>(options => 
-options.UseMySql (
-    builder.Configuration.GetConnectionString("MySqlConnectionString"),
+// Config DB
+builder.Services.AddDbContext<FastFilesContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
     Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
+
+// Scopes de los servicios
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
+// Cors
+app.UseCors("AllowAnyOrigin");
 
 app.UseHttpsRedirection();
 
+// Controllers
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 //Jwt
 app.UseAuthorization();
 app.UseAuthentication();
-
 
 app.Run();
 
